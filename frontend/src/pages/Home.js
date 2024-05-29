09e5b3847b0664c2e973f695e72ffd5b3b6f7d28
fartingwhales <chenxin1812@gmail.com>
2024-05-29 20:37:42 +0800

    styling login signup buttons

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Login from './Login';
 import Signup from './Signup';
+import './App.css';
 import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
