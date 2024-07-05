7c6981d2eead5c53d555e3457469de2c34aaec2c
chennieee <chenxin1812@gmail.com>
2024-07-05 09:56:35 +0800

    Make a small change to trigger new build

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 
-// pages & components
+// pages and components 
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
